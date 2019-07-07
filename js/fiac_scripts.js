93c230046ba39d488089ce2f0c1b6126654b2b76
mkornyev <mkornyev@gmail.com>
2019-07-08 07:48:40 +0900

    Multi-file uploads

--- js/fiac_scripts.js
+++ js/fiac_scripts.js
@@ -1,44 +1,66 @@
 
+
+
+// SUB:
+
+// - Create App Folder
+// - Upload F1
+//   Succ --> Upload F2 (Pass: fiac-select elementId / parent folder id /  )
+//             S --> Upload F3
+//             E --> Delete Folder / Rollback F1 & Del Folder
+//   Err --> Delete Folder 
+
+function fileUpload(elementId, parentId) {
+    var selectorId =  "fiac-select" + elementId;
+
+    var fileSelect = document.getElementById(selectorId);
+    var file = fileSelect.files[0];
+    
+    var formData = new FormData();
+    formData.append(file.name, file, file.name); // Selected File
+    formData.append('parent_id', parentId); // Parent
+
+    // API 
+    var uploadUrl = 'https://upload.box.com/api/2.0/files/content'; 
+    var uploadHeader = {
+        'Authorization': 'Bearer ekvdWNS6XzZmi4nYFaAuI8nvRWVpa1kB'
+    };
+
+    $.ajax({       
+        url: uploadUrl,
+        headers: uploadHeader,
+        type:'POST',
+        data: formData,
+        // Prevent JQuery from appending as querystring:
+        cache: false,
+        contentType: false,
+        processData: false,
+        // Feedback: 
+        success: function(data){
+            console.log("Upload Success (F" + elementId + "):");
+            console.log(data);
+        },
+        error: function(data){
+            console.log("Upload Error (F" + elementId + "):");
+            console.log(data);
+        }
+    });
+}
+
 // FIAC Script Start
 $(document).ready(function (e) {
 
-	var uploadButton = document.getElementById('fiac-upload-button');
-	var fileSelect = document.getElementById('fiac-select');
-
     $('#fiac-upload-form').on('submit',(function(e) {
-		
+
         e.preventDefault(); // Prevent default form submission
-        var files = fileSelect.files;
+
+        // Create Application Folder
+
+        fileUpload('1', '80802264662') // Upload File (selector, id)
+        fileUpload('2', '80802264662')
+		
         
-        var formData = new FormData();
-        formData.append(files[0].name, files[0], files[0].name); // Selected File
-        formData.append('parent_id', '80802264662'); // Parent
-
-        // API 
-		var uploadUrl = 'https://upload.box.com/api/2.0/files/content'; 
-		var uploadHeader = {
-		    'Authorization': 'Bearer ekvdWNS6XzZmi4nYFaAuI8nvRWVpa1kB'
-		};
-
-        $.ajax({       
-            url: uploadUrl,
-            headers: uploadHeader,
-            type:'POST',
-            data: formData,
-            // Prevent JQuery from appending as querystring:
-    		cache: false,
-    		contentType: false,
-    		processData: false,
-    		// Feedback: 
-            success: function(data){
-                console.log("Upload Success:");
-                console.log(data);
-            },
-            error: function(data, err){
-                console.log("Upload Error:");
-                console.log(data);
-            }
-        });
+        
     
     }));
 
