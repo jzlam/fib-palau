d3b753461d645a9108451a1902911663150e359f
mkornyev <mkornyev@gmail.com>
2019-07-07 14:07:39 +0900

    Contact Pg Added

--- js/fiac_scripts.js
+++ js/fiac_scripts.js
@@ -8,14 +8,14 @@ $(document).ready(function (e) {
     $('#fiac-upload-form').on('submit',(function(e) {
 		
         e.preventDefault(); // Prevent default form submission
-        uploadButton.innerHTML = 'Uploading...'; // UI Feedback 
         var files = fileSelect.files;
         
         var formData = new FormData();
         formData.append('fiac.pdf', files[0], 'fiac.pdf'); // Selected File
         formData.append('parent_id', '80802264662'); // Parent
 
-		var uploadUrl = 'https://upload.box.com/api/2.0/files/content';
+        // API 
+		var uploadUrl = 'https://upload.box.com/api/2.0/files/content'; 
 		var uploadHeader = {
 		    'Authorization': 'Bearer ekvdWNS6XzZmi4nYFaAuI8nvRWVpa1kB'
 		};
@@ -45,8 +45,3 @@ $(document).ready(function (e) {
     // Additional Scripts:
   
 });
-
-
-
-
-
